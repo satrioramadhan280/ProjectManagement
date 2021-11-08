d2ca1fce25a69cd035fa933f9e0ec28d7254770e
satrioramadhan280 <62045532+satrioramadhan280@users.noreply.github.com>
2021-11-08 11:18:40 +0700

    utility

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/admin/add', [AdminController::class, 'create']);
-Route::get('/admin/{user}', [AdminController::class, 'show']);
+
+Route::prefix('admin')->group(function () {
+    Route::get('/add', [AdminController::class, 'create']);
+    Route::get('/{user}', [AdminController::class, 'show']);
+});
