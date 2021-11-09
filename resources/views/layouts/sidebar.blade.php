dc487de1181bd8721015d4022fdbb9c2fbb703f3
kevinlorenzo10 <kevinlorenzo1919@gmail.com>
2021-11-09 14:43:01 +0700

    fix middleware

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,67 +7,79 @@
                     Dashboard
                 </a>
             </li>
-            @can('HDiv', 'HDept1', 'HDept2', 'HDept3', 'HDept4')
+            
+            @can('HDiv')
             <li class="nav-item">
                 <a class="nav-link" href="{{route('projects')}}">
                     <span data-feather="file"></span>
                     Projects
                 </a>
             </li>
-            @endcan
-            @can('HDiv', 'HDept1', 'HDept2', 'HDept3', 'HDept4')
+
             <li class="nav-item">
                 <a class="nav-link" href="#">
                     <span data-feather="user"></span>
                     Profile
                 </a>
             </li>
-            @endcannot
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span data-feather="bar-chart-2"></span>
-                    Reports
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span data-feather="layers"></span>
-                    Integrations
-                </a>
-            </li>
-        </ul>
+            @endcan
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>Saved reports</span>
-            <a class="link-secondary" href="#" aria-label="Add a new report">
-                <span data-feather="plus-circle"></span>
-            </a>
-        </h6>
-        <ul class="nav flex-column mb-2">
+            @can('HDept1')
             <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span data-feather="file-text"></span>
-                    Current month
+                <a class="nav-link" href="{{route('projects')}}">
+                    <span data-feather="file"></span>
+                    Projects
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <span data-feather="file-text"></span>
-                    Last quarter
+                    <span data-feather="user"></span>
+                    Profile
+                </a>
+            </li>
+            @endcan
+            <@can('HDept2')
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('projects')}}">
+                    <span data-feather="file"></span>
+                    Projects
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <span data-feather="file-text"></span>
-                    Social engagement
+                    <span data-feather="user"></span>
+                    Profile
+                </a>
+            </li>
+            @endcan
+            @can('HDept3')
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('projects')}}">
+                    <span data-feather="file"></span>
+                    Projects
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <span data-feather="file-text"></span>
-                    Year-end sale
+                    <span data-feather="user"></span>
+                    Profile
                 </a>
             </li>
+            @endcan
+            @can('HDept4')
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('projects')}}">
+                    <span data-feather="file"></span>
+                    Projects
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="user"></span>
+                    Profile
+                </a>
+            </li>
+            @endcan
         </ul>
     </div>
 </nav>
