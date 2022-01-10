b9f1fc951dff9350c8485ce0d068a1b8b6565607
kevinlorenzo10 <kevinlorenzo1919@gmail.com>
2022-01-10 17:29:03 +0700

    sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,15 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
     <div class="position-sticky pt-3">
-        {{Auth::user()->name}}
+        <div class="ml-auto mt-auto">
+            <li class="nav-item d-inline">
+                <img class="rounded-circle border border-3 ml-3 mr-1 d-inline" src="{{asset("uploads/users_photo/".Auth::user()->photo)}}" height="100px" width="100px" alt="">   
+                {{Auth::user()->name}}
+                <a class="nav-link ml-2" href="/user/{{Auth::user()->username}}/about">
+                    Edit Profile
+                </a>  
+            </li>
+        </div>
+        <hr>
         <ul class="nav flex-column">
             <li class="nav-item">
                 <a class="nav-link {{request()->is('home') ? 'active' : ''}}" aria-current="page" href="{{url('/home')}}">
@@ -17,10 +26,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{request()->is('*/about') ? 'active' : ''}}" href="/user/{{Auth::user()->username}}/about">
-                    <span data-feather="user"></span>
-                    Profile
-                </a>
+                
             </li>
             @endcanany
             @canany(['HDiv','HDept1', 'HDept2', 'HDept3', 'HDept4'])
